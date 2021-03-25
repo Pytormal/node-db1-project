4203520223beea62a45441e7a239ef2be0818391
AlexAndrew Mallory <pytormal@live.com>
2021-03-24 21:39:54 -0700

    starting model and router

--- api/server.js
+++ api/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const server = express();
 
+const accRouter = require('./accounts/accounts-router')
+
 server.use(express.json());
+server.use('/api/accounts',accRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({api: 'running'})
+})
 
 module.exports = server;
